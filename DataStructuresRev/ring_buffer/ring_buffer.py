9c39a142034c7690029d08992d1c92009a86c688
Joselyn Guaman <joselyng.97@gmail.com>
2021-11-30 21:21:12 -0500

    Completed_Buffer

--- DataStructuresRev/ring_buffer/ring_buffer.py
+++ DataStructuresRev/ring_buffer/ring_buffer.py
@@ -1,9 +1,35 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
-
+        self.i = 0 #counter for array
+        self.cap = capacity
+        self.a = [] #make a empty list
     def append(self, item):
-        pass
-
+        # check len of list a if less than capacity append to list
+        if len(self.a) < self.cap:
+            self.a.append(item)
+            print(self.i)
+            print(self.a)
+        elif self.i == (len(self.a)):
+            self.i = 0
+            self.a[self.i] = item
+            print(self.i)
+            print(self.a)
+        else:
+            self.a[self.i] = item
+            self.i += 1 #add one to the index/counter
+            print(self.i)
+            print(self.a)
     def get(self):
-        pass
+        print(self.a)
+buffer = RingBuffer(3)
+buffer.append(17)
+buffer.append(86)
+buffer.append(7)
+buffer.append(9)
+buffer.append(6)
+buffer.append(5)
+buffer.append(58)
+buffer.append(78)
+buffer.append(23)
+buffer.append(1)
+#buffer.get()
